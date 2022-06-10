4480b81eaa3f6676ca826ecfc726a05f25cd25bf
Eugene Eisenstein <eeisenst@gmail.com>
2022-06-10 14:00:01 -0400

    Convert part-of-speech CLI to hydra config

--- cai_manas/part_of_speech/cli.py
+++ cai_manas/part_of_speech/cli.py
@@ -1,44 +1,23 @@
 import sys
 import logging
-import argparse
+import hydra
 
 from .pos_tagger import PartOfSpeechTagger
 
 
-if __name__ == "__main__":
+@hydra.main(version_base="1.2", config_path="./cli.config", config_name="config")
+def main(cfg):
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
         datefmt="%m/%d/%Y %H:%M:%S",
         handlers=[logging.StreamHandler(sys.stdout)],
         level=logging.INFO,
     )
-    logger = logging.getLogger(__package__)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--tokenizer_name",
-        type=str,
-        default='olive-large',
-        help="Tokenizer name")
-    parser.add_argument(
-        "--model_cfg_name",
-        type=str,
-        default='albert-base-v2',
-        help="Huggingface model config name")
-    parser.add_argument(
-        "--model_ckpt",
-        type=str,
-        help="Fine-tuned model weights name in the data registry")
-    parser.add_argument(
-        "--verbose",
-        action="store_true",
-        help="Print debug logs")
-    args = parser.parse_args()
-
-    if args.verbose:
-        logger.setLevel(logging.DEBUG)
-
-    tagger = PartOfSpeechTagger(args.tokenizer_name, args.model_cfg_name, args.model_ckpt)
+    tagger = PartOfSpeechTagger(
+        cfg.model.tokenizer_name,
+        cfg.model.model_cfg_name,
+        cfg.model.model_ckpt)
 
     print("Interactive Tibetan part-of-speech tagging...")
     while True:
@@ -49,3 +28,7 @@ if __name__ == "__main__":
         res = tagger.tag(bo_text)
         for word, tag in zip(res["words"], res["tags"]):
             print(word, tag)
+
+
+if __name__ == "__main__":
+    main()
